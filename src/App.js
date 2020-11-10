d68536eb412968bc7dd2230831c49aa541625747
Abd-ul-Qayyum <a.qayyum1647@gmail.com>
2020-11-10 16:13:18 +0500

    scnd

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 // import logo from './logo.svg';
 import './App.css';
-import Dinner from 'dinner';
+import Dinner from './dinner.js';
 
 function App() {
     return ( <
         div className = "App" >
         <
-        dinner / >
+        Dinner / >
         <
         /div>
     );
