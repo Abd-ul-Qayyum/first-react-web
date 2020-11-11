787bd7eaaa86cfdf74888e4d1daa6e741e8e47db
Abd-ul-Qayyum <a.qayyum1647@gmail.com>
2020-11-11 10:20:09 +0500

    Update dinner.js

--- src/dinner.js
+++ src/dinner.js
@@ -3,8 +3,8 @@ import React from 'react';
 function Dinner(props) {
     return <div >
         <
-        h3 > We are serving { props.Dinner } < /h3> <
-        h3 > We are serving sweat { props.Dinner } < /h3> <
+        h3 > We are serving { props.dishName } < /h3> <
+        h3 > We are serving sweat { props.sweetDish } < /h3> <
         /div>
 }
 export default Dinner;
